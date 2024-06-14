2949ffaef1eac073f34ef2e18b60e599e673a3df
Eduardo Macetas <eduardo.macetas@gmail.com>
2024-06-13 20:27:34 -0500

    Test scenario: Error handling for form fields

--- tests/file-upload-test.spec.ts
+++ tests/file-upload-test.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect} from '@playwright/test';
 
 test('File Upload', async ({page}) => {
+  // Navigate to the specified URL.
   await page.goto('https://practice-automation.com/');
 
   // Click the button with the text 'File Upload' on the page.
@@ -15,6 +16,20 @@ test('File Upload', async ({page}) => {
   // Click the Upload button.
   await page.click("[id='upload-btn']")
 
-  // Verify that the ''Thank you for your message. It has been sent.' is visible.
+  // Verify that the 'Thank you for your message. It has been sent.' is visible.
   await page.getByText('Thank you for your message. It has been sent.').isVisible();
+})
+
+test('Error Handling for Form Fields', async ({page}) => {
+  // Navigate to the specified URL.
+  await page.goto('https://practice-automation.com/');
+
+  // Click the button with the text 'File Upload' on the page.
+  await page.getByText('File Upload').click();
+
+  // Click the Upload button.
+  await page.click("[id='upload-btn']")
+
+  // Verify that the 'One or more fields have an error. Please check and try again.' is visible.
+  await page.getByText('One or more fields have an error. Please check and try again.').isVisible();
 })
