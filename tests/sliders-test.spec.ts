0d5ebf984b012c8140e10671331bb4e4f08fc310
Eduardo Macetas <eduardo.macetas@gmail.com>
2024-06-20 19:49:31 -0500

    Move slider

---
+++ tests/sliders-test.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "@playwright/test";
+
+test("Move slider", async ({ page }) => {
+  // Navigate to the specified URL.
+  await page.goto("https://practice-automation.com/");
+
+  // Click the button with the text 'JavaScript Delays' on the page.
+  await page.getByText("Sliders").click();
+
+  // Locate the slider element.
+  const slider = await page.locator("[id='slideMe']");
+  // Calculate the slider's offset width.
+  const sliderOffsetWidth = await slider.evaluate((el) => {
+    return el.getBoundingClientRect().width;
+  });
+
+  // Simulate dragging the slider.
+  await slider.hover({ force: true, position: { x: 0, y: 0 } });
+  await page.mouse.down();
+  await slider.hover({ force: true, position: { x: sliderOffsetWidth, y: 0 } });
+  await page.mouse.up();
+
+  // Assert that the value element displays '100'.
+  await expect(page.locator("[id='value']")).toHaveText('100');
+});
